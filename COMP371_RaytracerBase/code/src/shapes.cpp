fa7d08c8bf242d20c35a73c12e287c4d462ca047
mzmddi <110358757+mzmddi@users.noreply.github.com>
2026-02-19 13:03:48 -0500

    finished implementing the extractInformation method in shapes

--- COMP371_RaytracerBase/code/src/shapes.cpp
+++ COMP371_RaytracerBase/code/src/shapes.cpp
@@ -3,13 +3,77 @@
 // ---INCLUDE---
 #include "shapes.h";
 
+using namespace std;
+
 // ---CODE----
 
 // Sphere methods
 
-sphere::sphere(string type, Vector3f center, float radius) : type_(type), center_(center), radius_(radius){};
-sphere::getType() const override {return type_};
+sphere::sphere(string type, Vector3f center, float radius) : type_(type), center_(center), radius_(radius) {};
+sphere::getType() const override{return type_};
+sphere::extractInformation(const nlohmann::json &j) override
+{
+    if (j.contains("type") && j.contains("radius") && j.contains("center") && j.contains("ka") && j.contains("kd") && j.contains("ks") && j.contains("pc") && j.contains("ac") && j.contains("dc") && j.contains("sc"))
+    {
+        this->type_ = "sphere";
+        this->center_ << j["center"][0].get<float>(), j["center"][1].get<float>(), j["center"][2].get<float>();
+        this->radius_ = j["radius"].get<float>();
+        this->ka_ = j["ka"].get<float>();
+        this->kd_ = j["kd"].get<float>();
+        this->ks_ = j["ks"].get<float>();
+        this->pc_ = j["pc"].get<float>();
+        this->ac_ << j["ac"][0] << j["ac"][1] << j["ac"][2];
+        this->dc_ << j["dc"][0] << j["dc"][1] << j["dc"][2];
+        this->sc_ << j["sc"][0] << j["sc"][1] << j["sc"][2];
+
+        if (j.contains("transform"))
+        {
+            this->transform_ << j["transform"][0], j["transform"][1], j["transform"][2], j["transform"][3],
+                j["transform"][4], j["transform"][5], j["transform"][6], j["transform"][7],
+                j["transform"][8], j["transform"][9], j["transform"][10], j["transform"][11],
+                j["transform"][12], j["transform"][13], j["transform"][14], j["transform"][15];
+        }
+    }
+    else
+    {
+        cout << "Sphere is missing some mandatory values." << endl;
+        exit(0)
+    }
+};
+;
 
 // Rectangle methods
-rectangle::rectangle(string type, Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4) : type_(type), p1_(p1), p2_(p2), p3_(p3), p4_(p4);
-rectangle::getType() const override {return type_};
+rectangle::rectangle() {};
+rectangle::getType() const override{return type_};
+rectangle::extractInformation(const nlohmann::json &j) override
+{
+    if (j.contains("type") && j.contains("p1") && j.contains("p2") && j.contains("p3") && j.contains("p4") && j.contains("ka") && j.contains("kd") && j.contains("ks") && j.contains("pc") && j.contains("ac") && j.contains("dc") && j.contains("sc"))
+    {
+        this->type_ = "rectangle";
+        this->p1_ << j["p1"][0].get<float>(), j["p1"][1].get<float>(), j["p1"][2].get<float>();
+        this->p2_ << j["p2"][0].get<float>(), j["p2"][1].get<float>(), j["p2"][2].get<float>();
+        this->p3_ << j["p3"][0].get<float>(), j["p3"][1].get<float>(), j["p3"][2].get<float>();
+        this->p4_ << j["p4"][0].get<float>(), j["p4"][1].get<float>(), j["p4"][2].get<float>();
+        this->ka_ = j["ka"].get<float>();
+        this->kd_ = j["kd"].get<float>();
+        this->ks_ = j["ks"].get<float>();
+        this->pc_ = j["pc"].get<float>();
+        this->ac_ << j["ac"][0] << j["ac"][1] << j["ac"][2];
+        this->dc_ << j["dc"][0] << j["dc"][1] << j["dc"][2];
+        this->sc_ << j["sc"][0] << j["sc"][1] << j["sc"][2];
+
+        if (j.contains("transform"))
+        {
+            this->transform_ << j["transform"][0], j["transform"][1], j["transform"][2], j["transform"][3],
+                j["transform"][4], j["transform"][5], j["transform"][6], j["transform"][7],
+                j["transform"][8], j["transform"][9], j["transform"][10], j["transform"][11],
+                j["transform"][12], j["transform"][13], j["transform"][14], j["transform"][15];
+        }
+    }
+    else
+    {
+        cout << "Rectangle is missing some mandatory values." << endl;
+        exit(0)
+    }
+};
+
--- COMP371_RaytracerBase/code/src/shapes.h
+++ COMP371_RaytracerBase/code/src/shapes.h
@@ -8,34 +8,60 @@ this way they can implement their own interset method, but we can call the same 
 // ---INCLUDES---
 #include <string>
 #include <Eigen/Core>
+using Eigen::Matrix4f;
 using Eigen::Vector3f;
 
 //---CODE---
 
-class shape {
-    public:
-        virtual string getType() const = "0";
+class shape
+{
+protected:
+    string type_;
+
+    float ka_;
+    float kd_;
+    float ks_;
+    Vector3f ka_;
+    Vector3f kd_;
+    Vector3f ks_;
+    float pc_;
+    // these are present in all shapes, so im putting it in the parent class
+    // but they are all related to color, so im defining them and assigning them a value from the json but,
+    //      they wont be used for assignment 3 yet since it does not require color yet.
+
+    Matrix4f transform_ = Matrix4f::Identity();
+    // the transform is optional, so it is not always there
+    // in order to make sure that the code works regardless if it is there or not,
+    //      i assigned to the transform the identity matrix, so that when it is used, it basically does nothing
+    //      but transform was extracted from the json, then this wouldnt be an identity matrix, and it would affect the calculations
+
+    virtual string getType() const = "0";
+    virtual void extractInformation(const nlohmann::json &j) {};
 };
 
-class sphere : public shape {
-    string type_;
+class sphere : public shape
+{
     Vector3f center_;
     float radius_;
+
 public:
-    sphere(string type, Vector3f center, float radius){}
+    sphere() {}
     // all the other methods for this class
-    string getType() const override;
+    string getType() const override {};
+    void extractInformation(const nlohmann::json &j) override {};
 };
 
-class rectangle : public shape {
-    string type_;
+class rectangle : public shape
+{
     Vector3f p1_;
     Vector3f p2_;
     Vector3f p3_;
     Vector3f p4_;
+
 public:
-    rectangle(string type, Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4){}
+    rectangle() {}
     // All the other methods for this class
     // in the assignmemnt, it says assume all points are coplanar
-    string getType() const override;
+    string getType() const override {};
+    void extractInformation(const nlohmann::json &j) override {};
 };
